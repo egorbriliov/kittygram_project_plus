ac6d726633ad955b7637c6635b6e76d9cdf3d157
Egor Briliov <88632284+egorbriliov@users.noreply.github.com>
2026-05-15 15:10:01 +0300

    Add Owner serializer and docstrings.

--- cats/serializers.py
+++ cats/serializers.py
@@ -1,10 +1,23 @@
+"""Serializers for the Cat model."""
 from rest_framework import serializers
 
-from .models import Cat
+from .models import Cat, Owner
 
 
 class CatSerializer(serializers.ModelSerializer):
+    """Serializer for the Cat model."""
 
     class Meta:
+        """Meta class for the CatSerializer."""
+
         model = Cat
         fields = ('id', 'name', 'color', 'birth_year')
+
+
+class OwnerSerializer(serializers.ModelSerializer):
+    """Serializer for the Owner model."""
+    class Meta:
+        """Meta class for the OwnerSerializer."""
+
+        model = Owner
+        fields = ('first_name', 'last_name')
