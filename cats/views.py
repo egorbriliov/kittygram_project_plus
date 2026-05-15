5aa48f681f7fad34515d429e128ac150230450c0
Egor Briliov <88632284+egorbriliov@users.noreply.github.com>
2026-05-15 15:11:48 +0300

    Add owner viewset.

--- cats/views.py
+++ cats/views.py
@@ -1,9 +1,18 @@
 from rest_framework import viewsets
 
-from .models import Cat
-from .serializers import CatSerializer
+from .models import Cat, Owner
+from .serializers import CatSerializer, OwnerSerializer
 
 
 class CatViewSet(viewsets.ModelViewSet):
+    """API endpoint that allows cats to be viewed or edited."""
+
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
+
+
+class OwnerViewSet(viewsets.ModelViewSet):
+    """API endpoint that allows owners to be viewed or edited."""
+
+    queryset = Owner.objects.all()
+    serializer_class = OwnerSerializer
