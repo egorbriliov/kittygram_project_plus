15e5e08ad9fa28aabeaee20dce5f77f22b04643b
Egor Briliov <88632284+egorbriliov@users.noreply.github.com>
2026-05-15 20:55:18 +0300

    Add data of related model by related serializer.

--- cats/serializers.py
+++ cats/serializers.py
@@ -1,30 +1,42 @@
 """Serializers for the Cat model."""
 from rest_framework import serializers
 
-from .models import Cat, Owner
+from .models import AchievementCat, Cat, Owner, Achievement
+
+
+class AchievementSerializer(serializers.ModelSerializer):
+    """Serializer for the Achievement model."""
+    class Meta:
+        model = Achievement
+        fields = ('id', 'name')
 
 
 class CatSerializer(serializers.ModelSerializer):
     """Serializer for the Cat model."""
-    owner = serializers.HyperlinkedRelatedField(many=False,
-                                                read_only=True,
-                                                view_name='owner-detail')
+    achievements = AchievementSerializer(many=True, required=False)
 
     class Meta:
-        """Meta class for the CatSerializer."""
-
         model = Cat
-        fields = ('id', 'name', 'color', 'birth_year', 'owner')
+        fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements')
+
+    def create(self, validated_data):
+        if 'achievements' not in self.initial_data:  # type: ignore
+            cat = Cat.objects.create(**validated_data)
+            return cat
+        achievements = validated_data.pop('achievements')
+        cat = Cat.objects.create(**validated_data)
+        for achievement in achievements:
+            current_achievement, status = Achievement.objects.get_or_create(
+                **achievement)
+            AchievementCat.objects.create(
+                achievement=current_achievement, cat=cat)
+        return cat
 
 
 class OwnerSerializer(serializers.ModelSerializer):
     """Serializer for the Owner model."""
-    cats = serializers.HyperlinkedRelatedField(many=True,
-                                               read_only=True,
-                                               view_name='cat-detail')
+    cats = serializers.StringRelatedField(many=True, read_only=True)
 
     class Meta:
-        """Meta class for the OwnerSerializer."""
-
         model = Owner
         fields = ('first_name', 'last_name', 'cats')
