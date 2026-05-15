29e939d2bde82e703b17092571fc933a18b1735d
Egor Briliov <88632284+egorbriliov@users.noreply.github.com>
2026-05-15 11:33:29 +0300

    Add new related model for cat: owner. Made migrations and has solved db confilct.

--- cats/models.py
+++ cats/models.py
@@ -1,11 +1,27 @@
+"""Models for the cats app."""
 from django.db import models
 
 
+class Owner(models.Model):
+    """Model representing a cat owner."""
+
+    first_name = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=128)
+
+    def __str__(self):
+        """Return a string representation of the Owner object."""
+        return f'{self.first_name} {self.last_name}'
+
+
 class Cat(models.Model):
+    """Model representing a cat."""
+
     name = models.CharField(max_length=16)
     color = models.CharField(max_length=16)
     birth_year = models.IntegerField()
-
+    owner = models.ForeignKey(
+        Owner, related_name='cats', on_delete=models.CASCADE)
 
     def __str__(self):
+        """Return a string representation of the Cat object."""
         return self.name
