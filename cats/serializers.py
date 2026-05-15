65714f5d9aa5441757f10f2950ac3dd3694fab5e
Egor Briliov <88632284+egorbriliov@users.noreply.github.com>
2026-05-15 15:44:28 +0300

    Add new HyperLinkedRelated field.

--- cats/serializers.py
+++ cats/serializers.py
@@ -6,18 +6,25 @@ from .models import Cat, Owner
 
 class CatSerializer(serializers.ModelSerializer):
     """Serializer for the Cat model."""
+    owner = serializers.HyperlinkedRelatedField(many=False,
+                                                read_only=True,
+                                                view_name='owner-detail')
 
     class Meta:
         """Meta class for the CatSerializer."""
 
         model = Cat
-        fields = ('id', 'name', 'color', 'birth_year')
+        fields = ('id', 'name', 'color', 'birth_year', 'owner')
 
 
 class OwnerSerializer(serializers.ModelSerializer):
     """Serializer for the Owner model."""
+    cats = serializers.HyperlinkedRelatedField(many=True,
+                                               read_only=True,
+                                               view_name='cat-detail')
+
     class Meta:
         """Meta class for the OwnerSerializer."""
 
         model = Owner
-        fields = ('first_name', 'last_name')
+        fields = ('first_name', 'last_name', 'cats')
