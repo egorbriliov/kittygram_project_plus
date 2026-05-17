ec3df4bdf3ecffd779e7974c8eba30affec39e68
Egor Briliov <88632284+egorbriliov@users.noreply.github.com>
2026-05-17 13:56:02 +0300

    Add new API route.

--- kittygram_plus/urls.py
+++ kittygram_plus/urls.py
@@ -2,11 +2,12 @@ from rest_framework.routers import DefaultRouter
 
 from django.urls import include, path
 
-from cats.views import CatViewSet, OwnerViewSet
+from cats.views import CatViewSet, OwnerViewSet, LightCatViewSet
 
 router = DefaultRouter()
-router.register('cats', CatViewSet)
+router.register('cats', CatViewSet, basename='cat')
 router.register('owners', OwnerViewSet)
+router.register(r'mycats', LightCatViewSet, basename='lightcats')
 
 urlpatterns = [
     path('', include(router.urls)),
