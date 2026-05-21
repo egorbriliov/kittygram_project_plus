3957bac942dc8f542a63980c348d0abaad4e3ad4
Egor Briliov <88632284+egorbriliov@users.noreply.github.com>
2026-05-21 16:45:22 +0300

    Add JWT token accses and SafeRouter.

--- kittygram_plus/urls.py
+++ kittygram_plus/urls.py
@@ -1,14 +1,19 @@
 from rest_framework.routers import DefaultRouter
-
 from django.urls import include, path
-
 from cats.views import CatViewSet, OwnerViewSet, LightCatViewSet
 
-router = DefaultRouter()
-router.register('cats', CatViewSet, basename='cat')
-router.register('owners', OwnerViewSet)
+
+class SafeDefaultRouter(DefaultRouter):
+    include_format_suffixes = False
+
+
+router = SafeDefaultRouter()
+router.register('cats', CatViewSet, basename='cats')
+router.register('owners', OwnerViewSet, basename='owners')
 router.register(r'mycats', LightCatViewSet, basename='lightcats')
 
 urlpatterns = [
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
     path('', include(router.urls)),
 ]
